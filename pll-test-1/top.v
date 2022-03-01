0275b9289df9c85b6fa59759d3c6088eef11a449
adrian chadd <adrian@freebsd.org>
2022-02-28 21:59:43 -0800

    pll-test-1: steal how I use clock buffers from the ntsc experiment
    In the NTSC tree I used a pair of GB's to buffer the clock output
    from the PLL block before it's distributed elswhere.

--- pll-test-1/top.v
+++ pll-test-1/top.v
@@ -21,10 +21,23 @@ module ntsc_out_top(
 	output clk_2x
 );
 
+	wire clk_out, clk_2x_out;
+
+	// Buffer between clock PLL output and consumers
+	SB_GB clk_out_buffer(
+		.USER_SIGNAL_TO_GLOBAL_BUFFER(clk_out),
+		.GLOBAL_BUFFER_OUTPUT(clk)
+	);
+
+	SB_GB clk_2x_out_buffer(
+		.USER_SIGNAL_TO_GLOBAL_BUFFER(clk_2x_out),
+		.GLOBAL_BUFFER_OUTPUT(clk_2x)
+	);
+
 	// Reset instance
 	reset_top reset_inst(
 		.NRST(NRST),
-		.clk(clk),
+		.clk(clk_out),
 		.pll_lock(pll_lock),
 		.reset(reset)
 	);
@@ -34,8 +47,8 @@ module ntsc_out_top(
 		.clk_12(clk_12),
 		.reset(reset),
 		.pll_lock(pll_lock),
-		.clk(clk),
-		.clk_2x(clk_2x)
+		.clk(clk_out),
+		.clk_2x(clk_2x_out)
 	);
 
 endmodule
